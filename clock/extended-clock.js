2116aaf1bb24fedac1b6295a3d1c7a88e5d003cc
vladimiregardt <vladimiregardt@gmail.com>
2017-11-23 16:51:18 +0300

    23.11/1

--- clock/extended-clock.js
+++ clock/extended-clock.js
@@ -1,7 +1,7 @@
 /**
  * Created by User Notebook on 22.11.2017.
  */
-
+    "use strict";
     // Класс "расширенные часы"
     /*
     function extend(Child, Parent) {
@@ -37,7 +37,38 @@
 
     };
     */ //(-)
+    /*
+    function ExtendedClock(options) {
 
+        Clock.apply(this, arguments);
+        this._precision = +options._precision || 1000;
+    }
 
+    ExtendedClock.prototype = Object.create(Clock.prototype);
+    ExtendedClock.prototype.constructor = ExtendedClock;
 
+    ExtendedClock.prototype.start = function () {
+        this._render();
+        var self = this;
+        setInterval(function () {
+            self._render();
+        }, this._precision);
+    };
+    */ //(+-)
+    /*
+    function ExtendedClock(options) {
+        Clock.apply(this, arguments);
+        this._precision = +options.precision || 1000;
+    }
 
+    ExtendedClock.prototype = Object.create(Clock.prototype);
+    ExtendedClock.prototype.constructor = ExtendedClock;
+
+    ExtendedClock.prototype.start = function () {
+        this._render();
+        var self = this;
+        this._timer = setInterval(function () {
+            self._render();
+        }, this._precision);
+    };
+    */ //(+-)
