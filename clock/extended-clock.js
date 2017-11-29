08d18bc8112482cc34e3f009e25838bc371b90ca
vladimiregardt <vladimiregardt@gmail.com>
2017-11-29 14:02:00 +0300

    29.11/1

--- clock/extended-clock.js
+++ clock/extended-clock.js
@@ -216,3 +216,36 @@
         }, this._precision);
     };
     */ //(+)
+
+function extend(Child, Parent) {
+    Child.prototype = inherit(Parent.prototype);
+    Child.prototype.constructor = Child;
+    Child.parent = Parent.prototype;
+}
+
+function inherit(proto) {
+    function F() {}
+    F.prototype = proto;
+    return new F();
+}
+
+    function ExtendedClock(options) {
+        Clock.apply(this, arguments);
+        this._precision = +options.precision || 1000;
+    }
+
+    ExtendedClock.prototype = Object.create(Clock.prototype);
+    ExtendedClock.prototype.constructor = ExtendedClock;
+
+    ExtendedClock.prototype.start = function () {
+        this._render();
+        var self = this;
+        this._timer = setInterval( function () {
+            self._render();
+        }, this._precision );
+    };
+
+    //(+)
+
+
+
