fdebf2500daed1b0be93433b39952471193907ed
vladimiregardt <vladimiregardt@gmail.com>
2017-12-24 19:57:00 +0300

    refactoring

--- Twitter/app.js
+++ Twitter/app.js
@@ -3,7 +3,17 @@
 "use strict";
 //--------------------------
 
-    searchTweets("star wars");
+var data = searchTweets("star wars")
+    .then(function (value) {
+        return value;
+    })
+     .catch(function (error) {
+         console.log('error', error);
+     });
+
+console.log(data);
+
+
 
  function searchTweets(q) {
 
@@ -19,11 +29,7 @@
         count: "4"
     };
 
-    function percentEncode(str) {
-        return encodeURIComponent(str).replace(/[!*()']/g, function (character) {
-            return '%' + character.charCodeAt(0).toString(16);
-        });
-    }
+
 
     function encode(object) {
         return Object
@@ -49,17 +55,14 @@
     }
 
     var parameterString =  toParametersString(encodeObject);
-
-    function signatureHTTPSString() {
-        var HTTPStr = "https://api.twitter.com/1.1/search/tweets.json";
-        return "GET&" + percentEncode(HTTPStr) + "&";
-    }
+    var parameterStringEncode = percentEncode(parameterString);
+    var stringHttps = signatureHTTPSString();
 
     function signatureBaseString() {
-        return signatureHTTPSString() + percentEncode(parameterString);
+        return stringHttps + parameterStringEncode;
     }
 
-    var SignatureBseString = signatureBaseString();
+    var SignatureBaseString = signatureBaseString();
 
     var secretObj = {
         ConsumerSecret: "yA1OfYOi8aDV4u4s0lA5WcGsbid0pmiUoG3a8Gl8gkuXL1KqSO",
@@ -70,21 +73,23 @@
         return Object
             .keys(object)
             .map( function(item) {
-                var value = object[item];
-                return percentEncode(value);
+                return percentEncode(object[item]);
             })
             .join("&");
     }
+
     var key = getSigningKey(secretObj);
-    var sha1 = percentEncode(CryptoJS.HmacSHA1(SignatureBseString, key).toString());
+    var sha1 = percentEncode(CryptoJS.HmacSHA1(SignatureBaseString, key).toString());
     var words = CryptoJS.enc.Hex.parse(sha1);
     var signature = CryptoJS.enc.Base64.stringify(words);
 
-    function getAuthorization() {
-        var consumerKey = percentEncode(signatureOptions.oauth_consumer_key);
-        var nonce = percentEncode(signatureOptions.oauth_nonce);
-        var timestamp = percentEncode(signatureOptions.oauth_timestamp);
-        var accessToken = percentEncode(signatureOptions.oauth_token);
+     var consumerKey = percentEncode(signatureOptions.oauth_consumer_key);
+     var nonce = percentEncode(signatureOptions.oauth_nonce);
+     var timestamp = percentEncode(signatureOptions.oauth_timestamp);
+     var accessToken = percentEncode(signatureOptions.oauth_token);
+
+    function getAuthorizationHeader() {
+
         return  'OAuth '                                                +
             'oauth_consumer_key="'          + consumerKey       + '", ' +
             'oauth_nonce="'                 + nonce             + '", ' +
@@ -95,18 +100,18 @@
             'oauth_version="1.0"'                                       ;
     }
 
-    var authorization = getAuthorization();
+    var authorization = getAuthorizationHeader();
 
-    var status = function (response) {
+    var checkStatus = function (response) {
         if (response.status !== 200) {
             return Promise.reject(new Error(response.statusText));
         }
         return Promise.resolve(response);
     };
-    var json = function (response) {
+    var toJson = function (response) {
         return response.json();
     };
-
+    /*
     fetch("/api/search/tweets.json?q="+ q +"&result_type=mixed&count=4", {
         method: "GET",
         headers: {"Authorization" :  authorization,
@@ -120,10 +125,44 @@
         .catch(function (error) {
             console.log('error', error);
         });
-    //Что бы получить данные необходимо вытаскивать их then? а как сохранять в глобальные переменные?
-    // или тоже в then создавать узлы?
+    */
+
+     var url = "/api/search/tweets.json?q="+ q +"&result_type=mixed&count=4";
+     var options = {
+         method: "GET",
+         headers: {"Authorization" :  authorization,
+         }
+     };
+
+     function fetchFromApi(url, options) {
+
+         return fetch(url, options)
+             .then(checkStatus)
+             .then(toJson);
+             /*.then(function (data) {
+                 //console.log('data', data);
+                 return data;
+             });
+             */
+     }
+
+     return fetchFromApi(url, options);
 
 }
+
+
+
+function percentEncode(str) {
+    return encodeURIComponent(str).replace(/[!*()']/g, function (character) {
+        return '%' + character.charCodeAt(0).toString(16);
+    });
+}
+
+function signatureHTTPSString() {
+    var HTTPStr = "https://api.twitter.com/1.1/search/tweets.json";
+    return "GET&" + percentEncode(HTTPStr) + "&";
+}
+
 
 
 
