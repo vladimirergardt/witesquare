5e586fe1fcb3457bbc3e6317b5d38ebcbda6f9dd
vladimiregardt <vladimiregardt@gmail.com>
2018-02-12 21:34:36 +0300

    refactoring

--- Twitter/server.js
+++ Twitter/server.js
@@ -1,8 +1,13 @@
+
+
 const express = require('express');
 const proxy = require('http-proxy-middleware');
 const path = require('path');
 
 const app = express();
+const app = express();
+app.use(express.static('public'));
+
 
 app.use('/api', proxy({
   target: 'https://api.twitter.com/1.1',
@@ -12,6 +17,10 @@ app.use('/api', proxy({
   }
 }));
 
+app.get('/twitter.css', (req, res) => {
+  res.sendFile(path.join(__dirname, 'twitter.css'))
+});
+
 app.get('/app.js', (req, res) => {
   res.sendFile(path.join(__dirname, 'app.js'))
 });
@@ -20,7 +29,9 @@ app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'twitter.html'))
 });
 
+
 app.listen(3000, function() {
   console.log('App listening at http://::%s', 3000)
 });
+
 
